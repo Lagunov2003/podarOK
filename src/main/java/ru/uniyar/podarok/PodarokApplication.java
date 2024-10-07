a24337a51c016f13573265ad3125bb0d2c9ea563
a.kudrinskiy <artem.tankist.2015@yandex.ru>
2024-10-07 21:49:45 +0300

    Стартовый проект

---
+++ src/main/java/ru/uniyar/podarok/PodarokApplication.java
@@ -0,0 +1,13 @@
+package ru.uniyar.podarok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodarokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodarokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/uniyar/podarok/PodarokApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.uniyar.podarok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodarokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
